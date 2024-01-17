557fae749045a7e826cca5ca5ef81ebddfb31eb5
jose <202213865h@utea.edu.pe>
2024-01-17 10:06:40 +0100

    commit

---
+++ EJERCICIOS01/Ejercicio1.java
@@ -0,0 +1,7 @@
+package EJERCICIOS01;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("jose  julian alcarraz ");
+    }
+}
---
+++ eje1.java
@@ -0,0 +1,3 @@
+public class eje1 {
+   public static 
+}
